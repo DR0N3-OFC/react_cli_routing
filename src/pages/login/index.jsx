949c41f94e6824aa2daffdc76d70a4bc2d80697e
Bruno Raphael Facundo <86325711+DR0N3-OFC@users.noreply.github.com>
2022-09-14 22:22:39 -0300

    Finalizando página de login e estilos

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,35 @@
-import { Link } from "react-router-dom";
+import { MdLock, MdEmail } from "react-icons/md";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Container, Column, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from "./styles";
 
 const Login = () => {
     return(<>
-        <h1>Login</h1>
-        <Link to="/">Voltar para home</Link>
+        <Header />
+        <Container>
+            <Column>
+                <Title>
+                    A plataforma para você aprender com experts, dominar as principais tecnologias 
+                    e entrar mais rápido nas empresas mais desejadas.
+                </Title>
+            </Column>
+            <Column>
+                <Wrapper>
+                    <TitleLogin>Faça o seu cadastro</TitleLogin>
+                    <SubtitleLogin>Faça o seu login e make the change._</SubtitleLogin>
+                    <form>
+                        <Input placeholder="E-mail" type="email" leftIcon={<MdEmail />} />
+                        <Input placeholder="Senha" type="password" leftIcon={<MdLock />} />
+                        <Button title="Entrar" variant="secondary" />
+                    </form>
+                    <Row>
+                        <EsqueciText>Esqueci minha senha</EsqueciText>
+                        <CriarText>Criar Conta</CriarText>
+                    </Row>
+                </Wrapper>
+            </Column>
+        </Container>
     </>)
 }
 
